c2c79a1eec04eb1ab107a89587820c27f32926d3
Camille Sun <cooclle@hotmail.com>
2021-02-26 17:59:16 -0800

    removed large files after reset

--- extract_features/FeatureGenerator.py
+++ extract_features/FeatureGenerator.py
@@ -1,19 +1,19 @@
 #Author: Jingwen Sun
 #This is a class used for extract features from a tiles_state_and_action list for chi/pon/kan model.
 #usage: 
-#   tiles_state_and_action = np.load('tiles_state_and_action_2021.npy')
-#   fg = FeatureGenerator(tiles_state_and_action)
+#   file_name = "tiles_state_and_action_2021_sample.json"
+#   fg = FeatureGenerator(filename)
 #   for idx,(x,y) in enumerate(fg.ChiFeatureGenerator()): code for training
 #   for idx,(x,y) in enumerate(fg.PonFeatureGenerator()): code for training
 #   for idx,(x,y) in enumerate(fg.KanFeatureGenerator()): code for training
 
 import numpy as np
+import json
 
 class FeatureGenerator:
-
-    def __init__(self, tiles_state_and_action):
-        self.tiles_state_and_action = tiles_state_and_action
-    
+    def __init__(self, filename):
+        self.filename = filename
+        
     def getPlayerTiles(self, player_tiles):
         closed_hand_136 = player_tiles['closed_hand:']
         open_hand_136 = player_tiles['open_hand']
@@ -41,13 +41,13 @@ class FeatureGenerator:
         
         return np.concatenate((closed_hand_feature,open_hand_feature,discarded_tiles_feature))
     
-    def getSelfTiles(self, i):
-        player_tiles = self.tiles_state_and_action[6][i]
+    def getSelfTiles(self, tiles_state_and_action):
+        player_tiles = tiles_state_and_action["player_tiles"]
         return self.getPlayerTiles(player_tiles)       
 
-    def getEnemiesTiles(self,i):
-        player_seat = self.tiles_state_and_action[0][i]
-        enemies_tiles_list = self.tiles_state_and_action[7][i]
+    def getEnemiesTiles(self,tiles_state_and_action):
+        player_seat = tiles_state_and_action["player_id"]
+        enemies_tiles_list = tiles_state_and_action["enemies_tiles"]
         if(len(enemies_tiles_list)==3):
             enemies_tiles_list.insert(player_seat, {})
         enemies_tiles_feature = np.empty((0, 34))
@@ -57,14 +57,14 @@ class FeatureGenerator:
             enemies_tiles_feature = np.concatenate((enemies_tiles_feature,player_tiles))
         return enemies_tiles_feature
         
-    def getDoraList(self, i):
-        dora_list = self.tiles_state_and_action[8][i]
+    def getDoraList(self, tiles_state_and_action):
+        dora_list = tiles_state_and_action["dora"]
         dora_feature = np.zeros((5, 34))
         for idx, val in enumerate(dora_list):
             dora_feature[idx][val//4] = 1
         return dora_feature
     
-    def getScoreList1(self,i):
+    def getScoreList1(self,tiles_state_and_action):
         def trans_score(score):
             feature = np.zeros((34))
             if score > 50000:
@@ -77,8 +77,8 @@ class FeatureGenerator:
                 feature[int(a)] = 1-alpha
             return feature
             
-        player_seat = self.tiles_state_and_action[0][i]
-        scores_list = self.tiles_state_and_action[9][i]
+        player_seat = tiles_state_and_action["player_id"]
+        scores_list = tiles_state_and_action["scores"]
         scores_feature = np.zeros((4, 34))
         for i in range(4):
             score = scores_list[player_seat]
@@ -87,7 +87,7 @@ class FeatureGenerator:
             player_seat %= 4
         return scores_feature        
         
-    def getBoard1(self, i):
+    def getBoard1(self, tiles_state_and_action):
         def wind(c):
             if c=='E' : 
                 return 27
@@ -97,12 +97,12 @@ class FeatureGenerator:
                 return 29
             if c=='N' : 
                 return 30
-        player_seat = self.tiles_state_and_action[0][i]
-        dealer_seat = self.tiles_state_and_action[1][i]
-        repeat_dealer = self.tiles_state_and_action[2][i]
-        riichi_bets = self.tiles_state_and_action[3][i]
-        player_wind = self.tiles_state_and_action[4][i]
-        prevailing_wind = self.tiles_state_and_action[5][i]
+        player_seat = tiles_state_and_action["player_id"]
+        dealer_seat = tiles_state_and_action["dealer"]
+        repeat_dealer = tiles_state_and_action["repeat_dealer"]
+        riichi_bets = tiles_state_and_action["riichi_bets"]
+        player_wind = tiles_state_and_action["player_wind"]
+        prevailing_wind = tiles_state_and_action["prevailing_wind"]
         
         dealer_feature = np.zeros((1, 34))
         dealer_feature[0][(dealer_seat+4-player_seat)%4] = 1
@@ -127,30 +127,31 @@ class FeatureGenerator:
             prevailing_wind_feature
         ))
         
-    def getGeneralFeature(self, i):
+    def getGeneralFeature(self, tiles_state_and_action):
         return np.concatenate((            
-            self.getSelfTiles(i), #(12,34)
-            self.getDoraList(i), #(5,34)
-            self.getBoard1(i), #(5,34)
-            self.getEnemiesTiles(i), #(36,34)
-            self.getScoreList1(i) #(4,34)
+            self.getSelfTiles(tiles_state_and_action), #(12,34)
+            self.getDoraList(tiles_state_and_action), #(5,34)
+            self.getBoard1(tiles_state_and_action), #(5,34)
+            self.getEnemiesTiles(tiles_state_and_action), #(36,34)
+            self.getScoreList1(tiles_state_and_action) #(4,34)
         ))
         
     def ChiFeatureGenerator(self):
-        for i in range(len(self.tiles_state_and_action[12])):
-            could_chi = self.tiles_state_and_action[12][i]
-            if could_chi == 1:
-                last_player_discarded_tile = self.tiles_state_and_action[10][i]
-                last_player_discarded_tile_feature = np.zeros((1, 34))
-                last_player_discarded_tile_feature[0][last_player_discarded_tile//4] = 1
-                x = np.concatenate((last_player_discarded_tile_feature,self.getGeneralFeature(i)))
-                    
-                action = self.tiles_state_and_action[15][i]
-                if action[0]=='Chi':
-                    y = 1
-                else:
-                    y = 0
-                yield x,y
+        with open(self.filename) as infile:
+            for line in infile:
+                tiles_state_and_action = json.loads(line)
+                could_chi = tiles_state_and_action["could_chi"]
+                last_player_discarded_tile = tiles_state_and_action["last_player_discarded_tile"]
+                action = tiles_state_and_action["action"]
+                if could_chi == 1:                    
+                    last_player_discarded_tile_feature = np.zeros((1, 34))
+                    last_player_discarded_tile_feature[0][last_player_discarded_tile//4] = 1
+                    x = np.concatenate((last_player_discarded_tile_feature,self.getGeneralFeature(tiles_state_and_action)))
+                    if action[0]=='Chi':
+                        y = 1
+                    else:
+                        y = 0
+                    yield x,y
                 
     def PonFeatureGenerator(self):
         def could_pon(closed_hand_136, last_discarded_tile):
@@ -163,23 +164,24 @@ class FeatureGenerator:
                 return True
             return False
         
-        for i in range(len(self.tiles_state_and_action[13])):
-            if self.tiles_state_and_action[13][i] == 1:
-                last_three_discarded_tile_list = self.tiles_state_and_action[11][i]
-                closed_hand_136 = self.tiles_state_and_action[6][i]['closed_hand:']
-                for last_discarded_tile in last_three_discarded_tile_list:
-                    if could_pon(closed_hand_136, last_discarded_tile):
-                        last_discarded_tile_feature = np.zeros((1, 34))
-                        last_discarded_tile_feature[0][last_discarded_tile//4] = 1
-                        x = np.concatenate((last_discarded_tile_feature,self.getGeneralFeature(i)))
-                        
-                        action = self.tiles_state_and_action[15][i]
-                        if action[0]=='Pon' and (last_discarded_tile in action[1]):
-                            y = 1
-                        else:
-                            y = 0
-                        
-                        yield x,y
+        with open(self.filename) as infile:
+            for line in infile:
+                tiles_state_and_action = json.loads(line)
+                last_three_discarded_tile_list = tiles_state_and_action["last_three_discarded_tile"]
+                closed_hand_136 = tiles_state_and_action["player_tiles"]['closed_hand:']
+                action = tiles_state_and_action["action"]
+                if tiles_state_and_action["could_pon"] == 1:
+                    for last_discarded_tile in last_three_discarded_tile_list:
+                        if could_pon(closed_hand_136, last_discarded_tile):
+                            last_discarded_tile_feature = np.zeros((1, 34))
+                            last_discarded_tile_feature[0][last_discarded_tile//4] = 1
+                            x = np.concatenate((last_discarded_tile_feature,self.getGeneralFeature(tiles_state_and_action)))
+                            if action[0]=='Pon' and (last_discarded_tile in action[1]):
+                                y = 1
+                            else:
+                                y = 0
+
+                            yield x,y
                         
     def KanFeatureGenerator(self):
         def could_minkan(closed_hand_136, last_discarded_tile):
@@ -206,46 +208,51 @@ class FeatureGenerator:
                 if count[tile//4]==3:
                     return True
             return False
-        for i in range(len(self.tiles_state_and_action[14])):
-            last_three_discarded_tile_list = self.tiles_state_and_action[11][i]
-            closed_hand_136 = self.tiles_state_and_action[6][i]['closed_hand:']
-            open_hand_136 = self.tiles_state_and_action[6][i]['open_hand']
-            action = self.tiles_state_and_action[15][i]
-            if self.tiles_state_and_action[14][i] == 1: #Minkan
-                for last_discarded_tile in last_three_discarded_tile_list:
-                    if could_minkan(closed_hand_136, last_discarded_tile):
-                        kan_type_feature = np.zeros((3, 34))
-                        kan_type_feature[0] = 1
-                        last_discarded_tile_feature = np.zeros((1, 34))
-                        last_discarded_tile_feature[0][last_discarded_tile//4] = 1
-                        x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(i)))
-                        
-                        if action[0]=='MinKan' and (last_discarded_tile in action[1]):
-                            y = 1
-                        else:
-                            y = 0
-                        yield x,y
-            else:
-                if could_ankan(closed_hand_136): #AnKan
-                    kan_type_feature = np.zeros((3, 34))
-                    kan_type_feature[1] = 1
-                    last_discarded_tile_feature = np.zeros((1, 34))
-                    x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(i)))
-                    
-                    if action[0]=='AnKan':
-                        y = 1
-                    else:
-                        y = 0
-                    yield x,y
+        
+        with open(self.filename) as infile:
+            for line in infile:
+                tiles_state_and_action = json.loads(line)
+                last_three_discarded_tile_list = tiles_state_and_action["last_three_discarded_tile"]
+                closed_hand_136 = tiles_state_and_action["player_tiles"]['closed_hand:']
+                open_hand_136 = tiles_state_and_action["player_tiles"]['open_hand']
+                action = tiles_state_and_action["action"]
+                if tiles_state_and_action["could_minkan"] == 1: #Minkan
+                    for last_discarded_tile in last_three_discarded_tile_list:
+                        if could_minkan(closed_hand_136, last_discarded_tile):
+                            kan_type_feature = np.zeros((3, 34))
+                            kan_type_feature[0] = 1
+                            last_discarded_tile_feature = np.zeros((1, 34))
+                            last_discarded_tile_feature[0][last_discarded_tile//4] = 1
+                            x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(tiles_state_and_action)))
+
+                            if action[0]=='MinKan' and (last_discarded_tile in action[1]):
+                                y = 1
+                            else:
+                                y = 0
+                            yield x,y
                 else:
-                    if could_kakan(closed_hand_136, open_hand_136): #KaKan
+                    if could_ankan(closed_hand_136): #AnKan
                         kan_type_feature = np.zeros((3, 34))
-                        kan_type_feature[2] = 1
+                        kan_type_feature[1] = 1
                         last_discarded_tile_feature = np.zeros((1, 34))
-                        x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(i)))
-                        
-                        if action[0]=='KaKan':
+                        x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(tiles_state_and_action)))
+
+                        if action[0]=='AnKan':
                             y = 1
                         else:
                             y = 0
                         yield x,y
+                    else:
+                        if could_kakan(closed_hand_136, open_hand_136): #KaKan
+                            kan_type_feature = np.zeros((3, 34))
+                            kan_type_feature[2] = 1
+                            last_discarded_tile_feature = np.zeros((1, 34))
+                            x = np.concatenate((kan_type_feature,last_discarded_tile_feature,self.getGeneralFeature(tiles_state_and_action)))
+
+                            if action[0]=='KaKan':
+                                y = 1
+                            else:
+                                y = 0
+                            yield x,y
+
+
