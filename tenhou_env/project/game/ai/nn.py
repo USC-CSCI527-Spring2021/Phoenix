9c2b7445ddbbc8d28133d7c0e14c5fc64c953766
邢宇 <xingyu@MacBook-Pro.local>
2021-03-07 15:38:21 -0800

    added pheonix ai, nn

---
+++ tenhou_env/project/game/ai/nn.py
@@ -0,0 +1,95 @@
+import game.ai.models as models
+import numpy as np
+input_shape = (None, 34, 1)   ######### fix input_shape afterwards
+
+class Chi:
+    def __init__(self, player):
+        self.player = player
+        self.model = models.make_or_restore_model(input_shape, "chi")
+    
+    def should_call_chi(self, tile_136, is_kamicha_discard):
+        features = self.getFeature()
+        p_donot, p_do = self.model.predict(np.expand_dims(features, axis=0))[0]
+        if p_do > p_donot:
+            return True, p_do
+        else:
+            return False, p_donot
+
+    def getFeature(self):
+        return np.random.randn(62, 34, 1)       #change this according to actual shape
+
+
+class Pon:
+    def __init__(self, player):
+        self.player = player
+        self.model = models.make_or_restore_model(input_shape, "pon")
+    def should_call_pon(self, tile_136, is_kamicha_discard):
+        features = self.getFeature()
+        p_donot, p_do = self.model.predict(np.expand_dims(features, axis=0))[0]
+        if p_do > p_donot:
+            return True, p_do
+        else:
+            return False, p_donot
+
+    def getFeature(self):
+        return np.random.randn(62, 34, 1)       #change this according to actual shape
+
+
+
+class Kan:
+    def __init__(self, input_shape, player):
+        self.player = player
+        self.model = models.make_or_restore_model(input_shape, "kan")
+
+    def should_call_kan(self, tile, open_kan, from_riichi=False):
+        features = self.getFeature()
+        p_donot, p_do = self.model.predict(np.expand_dims(features, axis=0))[0]
+        if p_do > p_donot:
+            return True, p_do
+        else:
+            return False, p_donot
+
+    def getFeature(self):
+        return np.random.randn(62, 34, 1)       #change this according to actual shape
+
+
+class Riichi:
+    def __init__(self, player):
+        self.player = player
+        self.model = models.make_or_restore_model(input_shape, "riichi")
+
+    def should_call_riichi(self):
+        features = self.getFeature()
+        p_donot, p_do = self.model.predict(np.expand_dims(features, axis=0))[0]
+        if p_do > p_donot:
+            return True, p_do
+        else:
+            return False, p_donot
+
+    def getFeature(self):
+        return np.random.randn(62, 34, 1)       #change this according to actual shape
+
+class Discard:
+    def __init__(self, player):
+        self.player = player
+        self.model = models.make_or_restore_model(input_shape, "discard")
+
+    def discard_tile(self, all_hands_136=None, closed_hands_136=None):
+        '''
+        The reason why these two should be input:
+        if the "discard" action is from "discard after meld", since we have decided to meld, what
+        to do next is to decide what to discard, but the meld has not happened (and could also be 
+        interrupted), so we could only use discard model based on supposed hands after melding
+
+        '''
+        features = self.getFeature(all_hands_136, closed_hands_136)
+        tile_to_discard = np.argmax(self.model.predict(np.expand_dims(features, axis=0))[0])
+        tile_to_discard_136 = [h for h in closed_hands_136 if h // 4 == tile_to_discard][-1] 
+        #if multiple tiles exists, return the one which is not red dora
+        return tile_to_discard_136
+                
+
+
+
+    def getFeature(self, open_hands_136, closed_hands_136):
+        return np.random.randn(62, 34, 1)       #change this according to actual shape
---
+++ tenhou_env/project/game/ai/phoenix_main.py
@@ -0,0 +1,110 @@
+from game.ai.nn import Chi, Pon, Kan, Riichi, Discard
+from game.ai.hand_builder import HandBuilder
+from mahjong.meld import Meld
+
+class Phoenix:
+    def __init__(self, player):
+        self.player = player
+        self.table = player.table
+
+        self.chi = Chi(player)
+        self.pon = Pon(player)
+        self.kan = Kan(player)
+        self.riichi = Riichi(player)
+        self.discard = Discard(player)
+        self.hand_builder = HandBuilder(player, self)
+        
+        self.erase_state()
+
+    def erase_state(self):
+        pass
+
+    def init_hand(self):
+        pass
+
+    def draw_tile(self):
+        pass
+
+    def discard_tile(self, discard_tile):
+        '''
+        return discarded_tile and with_riichi
+        '''
+        if discard_tile is not None:
+            return discard_tile, False
+        else:
+            return self.discard.discard_tile()
+
+
+    def try_to_call_meld(self, tile_136, is_kamicha_discard, meld_type):
+        # 1 pon
+        # 2 kan (it is a closed kan and can be send only to the self draw)
+        # 4 chi
+        # there is two return value, meldPrint() and discardOption(), 
+        # while the second would not be used by client.py
+        if meld_type & 4:
+            meld_chi, chi_score = self.chi.should_call_chi(tile_136, is_kamicha_discard)
+            meld_chi = Meld(meld_type="chi", tiles=meld_chi) if meld_chi else None
+        if meld_type & 1:
+            meld_pon, pon_score = self.pon.should_call_pon(tile_136, is_kamicha_discard)
+            meld_pon = Meld(meld_type="pon", tiles=meld_pon) if meld_pon else None
+
+
+        if meld_chi == None and meld_pon == None:
+            return None, None
+
+        if meld_chi and meld_pon:
+            meld = meld_chi if chi_score > pon_score else meld_pon
+        elif meld_chi:
+            meld = meld_chi
+        else:
+            meld = meld_pon
+
+        all_tiles_copy, meld_tiles_copy = self.player.tiles, self.player.meld_tiles
+        all_tiles_copy.append(tile_136)
+        meld_tiles_copy.append(meld)
+        closed_hand_copy = [item for item in all_tiles_copy if item not in meld_tiles_copy]
+        discard_option = self.player.discard(all_tiles_copy, closed_hand_copy)
+
+        return meld, discard_option
+
+    def should_call_kyuushu_kyuuhai(self):
+        pass
+
+
+    def should_call_win(self, tile, is_tsumo, enemy_seat=None, is_chankan=False):
+        # don't skip win in riichi
+        if self.player.in_riichi:
+            return True
+
+        # currently we don't support win skipping for tsumo
+        if is_tsumo:
+            return True
+
+        # fast path - check it first to not calculate hand cost
+        cost_needed = self.placement.get_minimal_cost_needed()
+        if cost_needed == 0:
+            return True
+
+        # 1 and not 0 because we call check for win this before updating remaining tiles
+        is_hotei = self.player.table.count_of_remaining_tiles == 1
+
+        hand_response = self.calculate_exact_hand_value_or_get_from_cache(
+            tile,
+            tiles=self.player.tiles,
+            call_riichi=self.player.in_riichi,
+            is_tsumo=is_tsumo,
+            is_chankan=is_chankan,
+            is_haitei=is_hotei,
+        )
+        assert hand_response is not None
+        assert not hand_response.error, hand_response.error
+        cost = hand_response.cost
+        return self.placement.should_call_win(cost, is_tsumo, enemy_seat)
+
+
+    @property
+    def enemy_players(self):
+        """
+        Return list of players except our bot
+        """
+        return self.player.table.players[1:]
