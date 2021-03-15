699f17ac48dbd7ece59cc48b85eef8b833dce162
wate123 <junlin0604@gmail.com>
2021-03-14 20:11:30 -0500

    add docker files wip

--- setup.py
+++ setup.py
@@ -11,12 +11,13 @@ setup(
     description='',
     include_package_data=True,
     install_requires=[
-        'google-cloud-storage==1.36.1',
         'tensorflow_transform==0.28.0',
         'tensorflow==2.4.1',
         'mahjong==1.2.0.dev5',
         'tensorboard==2.4.1',
         'apache-beam==2.28.0',
-        'apache-beam[GCP]==2.28.0',
+        'numpy',
     ],
+    scripts=['pipeline.py']
+
 )
