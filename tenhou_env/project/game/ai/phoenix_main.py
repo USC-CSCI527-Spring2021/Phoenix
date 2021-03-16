0bfbfaeeec0bbed3ec8136aacf08fef7fb9a40f5
Yu <xingy@usc.edu>
2021-03-15 21:23:42 -0700

    random discard debugged

--- tenhou_env/project/game/ai/phoenix_main.py
+++ tenhou_env/project/game/ai/phoenix_main.py
@@ -7,9 +7,12 @@ from mahjong.constants import DISPLAY_WINDS
 from mahjong.meld import Meld
 from mahjong.shanten import Shanten
 from mahjong.tile import TilesConverter
+from mahjong.hand_calculating.hand_config import HandConfig, OptionalRules
+from mahjong.hand_calculating.hand import HandCalculator
+from mahjong.hand_calculating.divider import HandDivider
+from mahjong.utils import is_chi, is_honor, is_man, is_pin, is_pon, is_sou, is_terminal, plus_dora, simplify
 from utils.cache import build_shanten_cache_key
-
-
+from utils.cache import build_estimate_hand_value_cache_key, build_shanten_cache_key
 class Phoenix:
     def __init__(self, player):
         self.player = player
@@ -23,11 +26,16 @@ class Phoenix:
         self.hand_builder = HandBuilder(player, self)
         self.shanten_calculator = Shanten()
         self.hand_cache_shanten = {}
+        self.placement = player.config.PLACEMENT_HANDLER_CLASS(player)
+        self.finished_hand = HandCalculator()
+        self.hand_divider = HandDivider()
 
         self.erase_state()
 
     def erase_state(self):
-        pass
+        self.hand_cache_shanten = {}
+        self.hand_cache_estimation = {}
+        self.finished_hand = HandCalculator()
 
     def init_hand(self):
         self.player.logger.debug(
@@ -49,49 +57,54 @@ class Phoenix:
         '''
         return discarded_tile and with_riichi
         '''
-        if discard_tile is not None:  # discard after meld
+        if discard_tile is not None:    #discard after meld
             return discard_tile, False
-        if self.player.is_open_hand:  # can not riichi
+        if self.player.is_open_hand:    #can not riichi
             return self.discard.discard_tile(), False
-        tile_to_discard = self.discard.discard_tile()
-        # e.g [0, 14, 30, 31, 45, 55, 73, 80, 84, 93, 96, 99, 119, 120]
-        # transform to [0,1,1.....] form
-        tiles = self.player.closed_hand[:]
-        tiles.remove(tile_to_discard)
-        closed_hand_34 = TilesConverter.to_34_array(tiles)
-        waiting, shanten = self.hand_builder.calculate_waits(closed_hand_34, self.player.tiles)
-        if shanten != 0:  # can not riichi
-            return tile_to_discard, False
+
+        shanten = self.calculate_shanten_or_get_from_cache(self.player.closed_hand)        
+        if shanten != 0:                #can not riichi
+            return self.discard.discard_tile(), False
         with_riichi, p = self.riichi.should_call_riichi()
         tile_to_discard = self.discard.discard_tile(with_riichi=with_riichi)
         return tile_to_discard, with_riichi
-            
+
     def try_to_call_meld(self, tile_136, is_kamicha_discard, meld_type):
         # 1 pon
         # 2 kan (it is a closed kan and can be send only to the self draw)
         # 4 chi
         # there is two return value, meldPrint() and discardOption(), 
         # while the second would not be used by client.py
-        meld_chi = None
-        meld_pon = None
-        if meld_type & 4:
-            meld_chi, chi_score = self.chi.should_call_chi(tile_136, is_kamicha_discard)
-            meld_chi = Meld(meld_type="chi", tiles=meld_chi) if meld_chi else None
-        elif meld_type & 1:
-            meld_pon, pon_score = self.pon.should_call_pon(tile_136, is_kamicha_discard)
-            meld_pon = Meld(meld_type="pon", tiles=meld_pon) if meld_pon else None
+        meld_chi, meld_pon = None, None
+        should_chi, should_pon = False, False
 
-        if not meld_chi and not meld_pon:
+
+        print(tile_136)
+        print(self.player.closed_hand)
+        melds_chi, melds_pon = self.get_possible_meld(tile_136, is_kamicha_discard)
+        if meld_type & 4:
+            should_chi, chi_score = self.chi.should_call_chi(tile_136, is_kamicha_discard)
+            #fix here: tiles_chi is now the first possible meld
+            assert melds_chi
+            tiles_chi = melds_chi[0]
+            meld_chi = Meld(meld_type="chi", tiles=tiles_chi) if meld_chi else None
+        elif meld_type & 1:
+            should_pon, pon_score = self.pon.should_call_pon(tile_136, is_kamicha_discard)
+            assert melds_pon
+            tiles_pon = melds_pon[0]
+            meld_pon = Meld(meld_type="pon", tiles=tiles_pon) if meld_pon else None
+
+        if not should_chi and not should_pon:
             return None, None
 
-        if meld_chi and meld_pon:
+        if should_chi and should_pon:
             meld = meld_chi if chi_score > pon_score else meld_pon
-        elif meld_chi:
+        elif should_chi:
             meld = meld_chi
         else:
             meld = meld_pon
 
-        all_tiles_copy, meld_tiles_copy = self.player.tiles, self.player.meld_tiles
+        all_tiles_copy, meld_tiles_copy = self.player.tiles[:], self.player.meld_tiles[:]
         all_tiles_copy.append(tile_136)
         meld_tiles_copy.append(meld)
         closed_hand_copy = [item for item in all_tiles_copy if item not in meld_tiles_copy]
@@ -139,7 +152,7 @@ class Phoenix:
         cost = hand_response.cost
         return self.placement.should_call_win(cost, is_tsumo, enemy_seat)
 
-    def calculate_shanten_or_get_from_cache(self, closed_hand_34: List[int], use_chiitoitsu: bool):
+    def calculate_shanten_or_get_from_cache(self, closed_hand_34: List[int], use_chiitoitsu=True):
         """
         Sometimes we are calculating shanten for the same hand multiple times
         to save some resources let's cache previous calculations
@@ -154,9 +167,148 @@ class Phoenix:
         self.hand_cache_shanten[key] = result
         return result
 
+
+    def calculate_exact_hand_value_or_get_from_cache(
+        self,
+        win_tile_136,
+        tiles=None,
+        call_riichi=False,
+        is_tsumo=False,
+        is_chankan=False,
+        is_haitei=False,
+        is_ippatsu=False,
+    ):
+        if not tiles:
+            tiles = self.player.tiles[:]
+        else:
+            tiles = tiles[:]
+
+        tiles += [win_tile_136]
+
+        additional_han = 0
+        if is_chankan:
+            additional_han += 1
+        if is_haitei:
+            additional_han += 1
+        if is_ippatsu:
+            additional_han += 1
+
+        config = HandConfig(
+            is_riichi=call_riichi,
+            player_wind=self.player.player_wind,
+            round_wind=self.player.table.round_wind_tile,
+            is_tsumo=is_tsumo,
+            options=OptionalRules(
+                has_aka_dora=self.player.table.has_aka_dora,
+                has_open_tanyao=self.player.table.has_open_tanyao,
+                has_double_yakuman=False,
+            ),
+            is_chankan=is_chankan,
+            is_ippatsu=is_ippatsu,
+            is_haitei=is_tsumo and is_haitei or False,
+            is_houtei=(not is_tsumo) and is_haitei or False,
+            tsumi_number=self.player.table.count_of_honba_sticks,
+            kyoutaku_number=self.player.table.count_of_riichi_sticks,
+        )
+
+        return self._estimate_hand_value_or_get_from_cache(
+            win_tile_136, tiles, call_riichi, is_tsumo, additional_han, config
+        )
+
+    def _estimate_hand_value_or_get_from_cache(
+        self, win_tile_136, tiles, call_riichi, is_tsumo, additional_han, config, is_rinshan=False, is_chankan=False
+    ):
+        cache_key = build_estimate_hand_value_cache_key(
+            tiles,
+            call_riichi,
+            is_tsumo,
+            self.player.melds,
+            self.player.table.dora_indicators,
+            self.player.table.count_of_riichi_sticks,
+            self.player.table.count_of_honba_sticks,
+            additional_han,
+            is_rinshan,
+            is_chankan,
+        )
+        if self.hand_cache_estimation.get(cache_key):
+            return self.hand_cache_estimation.get(cache_key)
+
+        result = self.finished_hand.estimate_hand_value(
+            tiles,
+            win_tile_136,
+            self.player.melds,
+            self.player.table.dora_indicators,
+            config,
+            use_hand_divider_cache=True,
+        )
+
+        self.hand_cache_estimation[cache_key] = result
+        return result
+
     @property
     def enemy_players(self):
         """
         Return list of players except our bot
         """
         return self.player.table.players[1:]
+
+
+    def get_possible_meld(self, tile, is_kamicha_discard):
+
+        closed_hand = self.player.closed_hand[:]
+
+        # we can't open hand anymore
+        if len(closed_hand) == 1:
+            return None, None
+
+        discarded_tile = tile // 4
+        closed_hand_34 = TilesConverter.to_34_array(closed_hand + [tile])
+
+        combinations = []
+        first_index = 0
+        second_index = 0
+        if is_man(discarded_tile):
+            first_index = 0
+            second_index = 8
+        elif is_pin(discarded_tile):
+            first_index = 9
+            second_index = 17
+        elif is_sou(discarded_tile):
+            first_index = 18
+            second_index = 26
+
+        if second_index == 0:
+            # honor tiles
+            if closed_hand_34[discarded_tile] == 3:
+                combinations = [[[discarded_tile] * 3]]
+        else:
+            # to avoid not necessary calculations
+            # we can check only tiles around +-2 discarded tile
+            first_limit = discarded_tile - 2
+            if first_limit < first_index:
+                first_limit = first_index
+
+            second_limit = discarded_tile + 2
+            if second_limit > second_index:
+                second_limit = second_index
+
+            combinations = self.hand_divider.find_valid_combinations(
+                closed_hand_34, first_limit, second_limit, True
+            )
+
+        if combinations:
+            combinations = combinations[0]        
+        # possible_melds = []
+        melds_chi, melds_pon = [], []
+        for best_meld_34 in combinations:
+            # we can call pon from everyone
+            if is_pon(best_meld_34) and discarded_tile in best_meld_34:
+                if best_meld_34 not in melds_pon:
+                    melds_pon.append(best_meld_34)
+
+            # we can call chi only from left player
+            if is_chi(best_meld_34) and is_kamicha_discard and discarded_tile in best_meld_34:
+                if best_meld_34 not in melds_chi:
+                    melds_chi.append(best_meld_34)
+
+        return melds_chi, melds_pon          
